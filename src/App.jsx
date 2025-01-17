e7fa82ebd570e427ac33e3d153ce4c7756bd80e1
creswell <trevorlini@hotmail.com>
2025-01-17 15:46:21 +1030

    Movie Card Component done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Hello World</p>
+      </div>
+      <div>
+        <p>Hello World</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
