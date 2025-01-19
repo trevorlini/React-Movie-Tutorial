ba4029a3b747518705c30f4eef279f05f385d8aa
creswell <trevorlini@hotmail.com>
2025-01-19 12:25:21 +1030

    page routing complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   const movieNumber = 1
 
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
+    </div>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,42 @@
-import MovieCard from "../components/MovieCard"
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
-function Home(){
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
 
   const movies = [
-    {id: 1, title: "John Wick", release_date: "2020"},
-    {id: 2, title: "Terminator", release_date: "1999"},
-    {id: 3, title: "The Matrix", release_date: "1998"},
-  ]
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "1999" },
+    { id: 3, title: "The Matrix", release_date: "1998" },
+  ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
-  return <div className="home">
-    <form onSubmit={handleSearch} className="search-form">
-      <input type="text" placeholder="Search for movies..." className="search-input"/>
-      <button type="submit" className="search-button">Search</button>
-    </form>
-    <div className="movies-grid">
-      {movies.map((movie) => 
-      (<MovieCard movie={movie} key={movie.id}/>
-      ))}
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+        <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
     </div>
-  </div>
+  );
 }
 
-export default Home
+export default Home;
