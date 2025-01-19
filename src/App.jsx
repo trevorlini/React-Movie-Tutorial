f501896c93ae6916509f94a0b0aa5c57828c4eba
creswell <trevorlini@hotmail.com>
2025-01-19 12:51:40 +1030

    added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./css/App.css";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import { Routes, Route } from "react-router-dom";
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,3 +1,5 @@
+import "../css/Favorites.css";
+
 function Favorites() {
   return <div className="favorites-empty">
     <h2>No Favorite Movies Yet</h2>
