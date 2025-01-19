ebb5df8453e7a531642c88d13d5fea738a95b366
creswell <trevorlini@hotmail.com>
2025-01-19 16:15:50 +1030

    added api

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "dd0e6d993502fafa0f179aa3747559b5";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async () => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
